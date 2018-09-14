84ceef292459c69918a8dfcb0f8fa6a6deccedaa
hiteshkothari <30888378+hiteshkothari@users.noreply.github.com>
2018-09-15 00:21:04 +0530

    Add files via upload

---
+++ BudXv.3/js/budx.js
@@ -0,0 +1,14 @@
+
+// if(window.scroll())addClass("fixed");
+function register(registerForm){
+	
+}
+
+function login(loginForm){
+	if(loginForm.email =="hitesh390@gmail.com"){
+		if(loginForm.password == "hitesh"){
+			location.replace("dashboard.html");
+		}
+	}
+
+}
